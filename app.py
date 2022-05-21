301e9748036f47606cdce3abb6eebb025af8e376
yolandahjohnson <100816778+yolandahjohnson@users.noreply.github.com>
2022-05-21 12:06:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ mongo = PyMongo(app)
 @app.route("/")
 def index():
     mars = mongo.db.mars.find_one()
-    return render_template("index.html", mars=mars)
+    return render_template("index2.html", mars=mars)
 
 @app.route("/scrape")
 def scrape():
     mars = mongo.db.mars
     mars_data = scraping.scrape_all()
-    mars.update({},mars_data, upsert=True)
+    mars.update_one({}, {"$set":mars_data}, upsert=True)
     return redirect('/', code=302)
     
 if __name__ == "__main__":
